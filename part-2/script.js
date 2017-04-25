61198e70302ad7c2131b003e00309346ebba5b86
RAN-PC\Ran Kuang <rkuang@id.iit.edu>
2017-04-25 13:33:26 -0500

    Add the div boxes, but the color is the opposite from the demo

--- part-2/script.js
+++ part-2/script.js
@@ -1,1 +1,13 @@
 // Do your work in this file.
+
+for (var i = 1; i <= 196; i++) {
+    var remainder = i % 2;
+    switch(remainder) {
+        case 0:
+            $("<div>").addClass("box type-1").appendTo(document.body);
+            break;
+        case 1:
+            $("<div>").addClass("box type-2").appendTo(document.body);
+            break;
+    }
+}
